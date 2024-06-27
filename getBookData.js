ee0964fe56adbfec8ff8c1662f21845ebd7cd0f1
Alexander Sukach <alexsukach@gmail.com>
2024-06-27 13:43:52 -0400

    second

--- getBookData.js
+++ getBookData.js
@@ -1,1 +1,15 @@
-document.getElementById('bookForm').addEventListener
+document.getElementById('bookForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const publisher = document.getElementById('publisher').value;
+    const subject = document.getElementById('subject').value;
+    const isbn = document.getElementById('isbn').value;
+});
+
+
+async function fetchBookData(params) {
+    let query = '';
+    if (params.title) query += `intitle:${encodeURIComponent(params.title)} `;
+
+}
